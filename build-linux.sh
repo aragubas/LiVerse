adc0e29dc35b864137588e72cabff67df21d3e70
Aragubas <47148297+aragubas@users.noreply.github.com>
2024-07-09 12:49:51 -0300

    Fix horrible script with my paths lol

--- build-linux.sh
+++ build-linux.sh
@@ -1,2 +1,41 @@
 #!/bin/bash
-cmake -DCMAKE_TOOLCHAIN_FILE=/home/aragubas/Development/Tools/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Debug -S/home/aragubas/Development/LiVerse -B/home/aragubas/Development/LiVerse/build -G Ninja && cmake --build /home/aragubas/Development/LiVerse/build --parallel 6
+vcpkg_path=undefined
+
+#
+#   Check if .vcpkg_path file exists
+#
+if [ -f .vcpkg_path ]; then
+    vcpkg_path="$(cat .vcpkg_path | tr -d '\n')"
+else
+    echo E: Could not find .vcpkg_path
+    echo Make sure to create .vcpkg_path with path to your vcpkg installation
+    exit 1
+fi
+
+echo vcpkg path set to: \"$vcpkg_path\"
+#
+#   Check if path specified in vcpkg_path exists
+#
+if [ ! -d $vcpkg_path ]; then
+    echo E: Path specified in .vcpkg_path doesn\'t exist
+    echo Please check if there\'s any typo or if the path specified in .vcpkg_path is valid.
+    exit 1
+fi
+
+#
+#   Check if vcpkg executable inside vcpkg_path exists
+#
+vcpkg_executable="$vcpkg_path"vcpkg
+if [ ! -f $vcpkg_executable ]; then
+    echo E: Could not find vcpkg executable inside vcpkg_path
+    echo Make sure vcpkg_path ends with slash or if it\'s pointing to the correct path
+    echo tried $vcpkg_executable
+    exit 1
+fi
+
+# 
+#   Get current directory
+#
+current_path=$(pwd)
+
+cmake -DCMAKE_TOOLCHAIN_FILE="$vcpkg_path"/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Debug -S"$current_path" -B"$current_path"/build -G Ninja && cmake --build "$current_path"/build --parallel 6
